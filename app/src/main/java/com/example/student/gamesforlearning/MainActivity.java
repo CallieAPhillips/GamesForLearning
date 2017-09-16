ab628ecc77cee81373b37e614a58accfdad17570
Callie Phillips <callie.phillips@bti360.com>
2017-09-15 22:37:15 -0400

    start screen doesn't go away anymore

--- app/src/main/java/com/example/student/gamesforlearning/MainActivity.java
+++ app/src/main/java/com/example/student/gamesforlearning/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void startGame(View view){
         Intent intent = new Intent(this, Game.class);
-        finish();
+        //finish();
         startActivity(intent);
     }
 
