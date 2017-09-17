ba19ddc6e217d8e43f8f4ac3c71c7a852cb8da5a
Callie Phillips <callie.phillips@bti360.com>
2017-09-17 00:25:37 -0400

    got blending colors to work

--- app/src/main/java/com/example/student/gamesforlearning/Game.java
+++ app/src/main/java/com/example/student/gamesforlearning/Game.java
@@ -1,15 +1,24 @@
 package com.example.student.gamesforlearning;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Game extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ImageButton mColorButton1;
+    private ImageButton mColorButton2;
+    private ImageButton mColorResultButton;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -38,8 +47,33 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        mColorButton1 = (ImageButton) findViewById(R.id.colorButton1);
+        mColorButton2 = (ImageButton) findViewById(R.id.colorButton2);
+        mColorResultButton = (ImageButton) findViewById(R.id.colorResultButton);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public void blendColors(View view){
+        int color1 = ((ColorDrawable)mColorButton1.getBackground()).getColor(); //color of first box
+        int color2 = ((ColorDrawable)mColorButton2.getBackground()).getColor(); //color of second box
+        int blendedColor = blendColors(color1, color2, 0.5f); //find blended color
+        mColorResultButton.getBackground().setColorFilter(blendedColor, PorterDuff.Mode.SRC_ATOP); //change color of result box
+    }
+
+    /**
+     * Blend {@code color1} and {@code color2} using the given ratio.
+     *
+     * @param ratio of which to blend. 1.0 will return {@code color1}, 0.5 will give an even blend,
+     *              0.0 will return {@code color2}.
+     */
+    private static int blendColors(int color1, int color2, float ratio) {
+        final float inverseRation = 1f - ratio;
+        float r = (Color.red(color1) * ratio) + (Color.red(color2) * inverseRation);
+        float g = (Color.green(color1) * ratio) + (Color.green(color2) * inverseRation);
+        float b = (Color.blue(color1) * ratio) + (Color.blue(color2) * inverseRation);
+        return Color.rgb((int) r, (int) g, (int) b);
     }
 
     @Override
